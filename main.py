c94a992af3e4cf90030a0e42cccf9c6868fd105b
Ronikusu Arawaka <156134393+kitsuniii@users.noreply.github.com>
2024-05-18 08:21:35 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
+import openai
 
+openai.api_key = "sk-proj-Fdtx4LwJpXlcFzdQWKWOT3BlbkFJLdpeyVJQX10wIPiaIwBN"
+
+def chat_with_gpt(prompt)
+    responce = openai.ChatCompletion.create(
+        model=gpt-3.5-turbo
+        messages=[{"role": "user", "content": prompt}]
+    )
+
+    return responce.choices[0].message.content.strip()
+
+if __name__ == "_main_":
+    while True:
+        if user_input.lower()in["quit", "exit", "bye"]:
+            break
+
+            responce = chat_with_gpt(user_input)
+            print("chatbot: ", response)
+
+            
